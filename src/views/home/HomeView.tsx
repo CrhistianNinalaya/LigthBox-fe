ab850e3f06be80a447338cdaf7118c86d1c25c81
angelnj11 <angelnj733@gmail.com>
2025-06-28 15:19:36 -0500

    carouselMovie no me salia el filtro y me olvide poner otra rama

--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,30 +1,36 @@
+'use client'
 import Carrusel from '@/components/Carrousel/Carrousel'
 import FilterByLocationAndDate from '@/components/FilterByLocationAndDate/FilterByLocationAndDate'
-import { StaticImport } from 'next/dist/shared/lib/get-img-props'
+import MovieCarousel from '@/components/MovieCarousel/MovieCarousel'
 
 export type SedeType = {
-	id: number
-	name: string
+  id: number
+  name: string
 }
 
 export type MovieType = {
-	link: string | StaticImport
+  movieId: number
+  titulo: string
+  imagenUrl: string
 }
+
 export interface HomeProps {
-	sedes: SedeType[]
-	movies: MovieType[]
+  sedes: SedeType[]
+  movies: MovieType[]
 }
 
 const HomeView = ({ sedes, movies }: HomeProps) => {
-	return (
-		<div className="w-full">
-			<FilterByLocationAndDate sedes={sedes} />
-			<Carrusel movies={movies} />
-			<div className={'px-10'}>
-				<h4 className={'text-2xl text-black'}>CARTELERA</h4>
-			</div>
-		</div>
-	)
+  return (
+    <div className="w-full">
+      <FilterByLocationAndDate sedes={sedes} />
+      <Carrusel movies={movies} />
+      <br />
+      <div className={'px-10'}>
+        <h4 className="text-3xl font-bold text-center text-gray-800 tracking-wide mb-6">CARTELERA</h4>
+        <MovieCarousel />
+      </div>
+    </div>
+  )
 }
 
 export default HomeView
